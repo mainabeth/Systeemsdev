f915f9fc65eb3e453fa7ed8edbf7ef1f824e0e1f
Mainabeth <mainabethy@gmail.com>
2022-12-19 12:13:17 +0300

    created admin middleware

--- reservation-system/routes/web.php
+++ reservation-system/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware(['auth', 'admin'])->name('admin.')->prefix('admin')->group(function() {
+    Route::get('/', [AdminController::class, 'index'])->name('index');
+});
+
+
+
+require __DIR__.'/auth.php';
